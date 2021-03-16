22e6af51ff07343609b1f18bb1308fb4bbf9d0c3
varune123 <varune23@hotmail.com>
2021-03-16 20:32:20 +0000

    models created

--- App/models.py
+++ App/models.py
@@ -16,5 +16,55 @@ class MyPokemon(db.Model):
 
 ## Create a User Model
 ## must have set_password, check_password and to Dict
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
+    def toDict(self):
+      return {
+        'id': self.id,
+        'username': self.username,
+        'email': self.email,
+        'password': self.password
+      }
+
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password, method='sha256')
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+class Pokemon(db.Model):
+    pid = db.Column('pid', db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    attack = db.Column(db.Integer)
+    defense = db.Column(db.Integer)
+    hp = db.Column(db.Integer)
+    height = db.Column(db.Integer)
+    sp_attack = db.Column(db.Integer)
+    sp_defense = db.Column(db.Integer)
+    speed = db.Column(db.Integer)
+    type1 = db.Column(db.String(50))
+    type2 = db.Column(db.String(50))
+    weight = db.Column(db.Float)
+
+    def toDict(self):
+        return {
+            'pid':self.pid,
+            'name':self.name,
+            'attack':self.attack,
+            'defense':self.defense,
+            'hp':self.hp,
+            'height':self.height,
+            'sp_attack':self.sp_attack,
+            'sp_defense':self.sp_defense,
+            'speed':self.speed,
+            'type1':self.type1,
+            'type2':self.type2,
+            'weight':self.weight
+        }
 ## Create a Pokemon Model
