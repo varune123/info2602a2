985b73c875df6d5a4ef3f5b27fe2d18b6fb3462d
varune123 <varune23@hotmail.com>
2021-03-17 00:10:18 +0000

    make list into dictionary and add to database

--- App/initDB.py
+++ App/initDB.py
@@ -4,9 +4,12 @@ import csv
 db.create_all(app=app)
 
 # add code to parse csv, create and save pokemon objects
+data = []
+
 with open('/workspace/info2602a2/App/pokemon.csv', 'r') as file:
     reader = csv.reader(file)
+    next(reader)
     for row in reader:
-        print(row)
-
+        data.append(row)
+print(data[0])
 # replace any null values with None to avoid db errors
